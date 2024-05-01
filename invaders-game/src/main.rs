64704c5a36159c7969ebe43604feb91db3cc131b
xipengjin <jinxp18@gmail.com>
2024-05-01 15:34:58 -0400

    initial commit

---
+++ invaders-game/src/main.rs
@@ -0,0 +1,49 @@
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use rusty_audio::Audio;
+use std::{
+    error::Error,
+    io::{self},
+    time::{Duration, Instant},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    for item in &["explode", "lose", "move", "pew", "startup", "win"] {
+        audio.add(item, &format!("audio/{}.wav", item));
+    }
+    audio.play("startup");
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Game Loop
+    'gameloop: loop {
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    },
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    // clean up
+    stdout.execute(Show)?; // show cursor
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    audio.wait();
+    Ok(())
+}
